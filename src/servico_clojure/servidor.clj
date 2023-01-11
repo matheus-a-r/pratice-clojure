fabac27aadf984999d90efa7890cf0af1886f426
Matheus Alves <matalves12345@gmail.com>
2023-01-11 13:04:20 -0300

    endpoint para criar e consultar tarefas

--- src/servico_clojure/servidor.clj
+++ src/servico_clojure/servidor.clj
@@ -3,6 +3,23 @@
             [io.pedestal.http :as http]
             [io.pedestal.test :as test]))
 
+(def store (atom {}))
+
+(defn lista-tarefas [request]
+  {:status 200 :body @store})
+
+(defn criar-tarefa-mapa [uuid nome status]
+  {:id uuid :nome nome :status status})
+
+(defn criar-tarefa [request]
+  (let [uuid (java.util.UUID/randomUUID)
+        nome (get-in request [:query-params :nome])
+        status (get-in request [:query-params :status])
+        tarefa (criar-tarefa-mapa uuid nome status)]
+    (swap! store assoc uuid tarefa)
+    {:status 200 :body {:mensagem "Tarefa registrada com sucesso!"
+                        :tarefa tarefa}}))
+
 (defn funcao-hello [request]
   {:status 200 :body (str "Hello World " (get-in request [:query-params :name] "Everybody"))})
 
@@ -10,7 +27,9 @@
 ;coleçao recebe varios vetores
 ;cada vetor eh uma rota
 (def routes (route/expand-routes
-              #{["/hello" :get funcao-hello :route-name :hello-world]}))
+              #{["/hello" :get funcao-hello :route-name :hello-world]
+                ["/tarefa" :post criar-tarefa :route-name :criar-tarefa]
+                ["/tarefa" :get lista-tarefas :route-name :lista-tarefas]}))
 
 
 ;configuracoes do projeto
@@ -30,6 +49,13 @@
 
 (start-server)
 (println (test-request :get "/hello?name=Matheus"))
+(println (test-request :post "/tarefa?nome=Ler&status=pendente"))
+(println (test-request :post "/tarefa?nome=Correr&status=feito"))
+
+(println "Listando todas as tarefas:")
+(println (test-request :get "/tarefa"))
+
+(println @store)
 
 
 
